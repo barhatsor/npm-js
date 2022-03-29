3bea0939a2f94b15ed89b87407c9a5f464c6858d
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2022-03-29 14:18:36 +0300

    Update npm.js

--- npm.js
+++ npm.js
@@ -1,2 +1,34 @@
 
-
+async function npm(packageJSON) {
+  
+  let packages = {};
+  
+  getPackages(packageJSON, packages);
+  
+  return packages;
+  
+}
+
+async function getPackages(packageNames, packageObj) {
+  
+  packageNames.forEach(packageName => {
+  
+    const package = await getPackage(packageName);
+    packages[packageName] = package;
+    
+    getPackages(package.dependencies, packageObj);
+    
+  });
+  
+}
+
+async function getPackage(package, version) {
+  
+  let resp = await axios.get(cors + 'https://registry.npmjs.org/' + name);
+  
+  if (version === 'latest') return resp['dist-tags'].latest;
+  
+  return resp.versions[version];
+  
+}
+
